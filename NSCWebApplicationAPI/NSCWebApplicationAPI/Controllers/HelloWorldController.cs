795cc298a05f8acb619d3e6e9d285e266ad18677
mantrisurya <mantri.surya.1@gmail.com>
2023-04-28 21:29:45 +0530

    Add files via upload

---
+++ NSCWebApplicationAPI/NSCWebApplicationAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSCWebApplicationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : Controller
+    {
+        // GET: HomeController
+        //[HttpGet(Name = "GetHelloWorld")]
+        public string Index()
+        {
+            return "HelloWorld!";
+        }
+    }
+}
